02d4f01d85547136c934b714c04f9efbca76ef29
Adesoji Adewumi <adesoji_adewumi@yahoo.com>
2025-12-23 19:18:33 +0100

    Create index.js
    Added  src/index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import generateRoute from "./routes/generate.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/generate", generateRoute);
+
+app.get("/", (req, res) => {
+  res.send("FollowUpCloser Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
+              
