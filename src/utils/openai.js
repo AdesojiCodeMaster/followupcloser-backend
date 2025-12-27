cd93f337344ff7a296dc96455ef3ce2d1c5c85ed
Adesoji Adewumi <adesoji_adewumi@yahoo.com>
2025-12-27 16:57:38 +0100

    Update openai.js
    Added max_tokens

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,7 +1,9 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
+  max_tokens: 120,
+
 });
 
 export default openai;
